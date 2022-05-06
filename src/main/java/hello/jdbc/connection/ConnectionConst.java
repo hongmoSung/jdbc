b15ca63df7c533e2a938f8a70a2fd48e202fa790
hongmoSung <tesung30@naver.com>
2022-05-06 18:35:17 +0900

    feat: JDBC 이해
    - 데이터베이스 연결

---
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,9 @@
+package hello.jdbc.connection;
+
+public abstract class ConnectionConst {
+
+    public static final String URL = "jdbc:mariadb://localhost:3306";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "1234";
+
+}
