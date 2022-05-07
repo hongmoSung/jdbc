434f78cbb20f706724e03cfc758261edaf4794b4
hongmoSung <tesung30@naver.com>
2022-05-07 11:49:43 +0900

    feat: 커넥션풀과 데이터소스 이해
    - DataSource 예제1 - DriverManager

---
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,38 @@
+package hello.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void driverManager() throws SQLException {
+        Connection connection1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection connection2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        log.info("connection = {}, class = {}", connection1, connection1.getClass());
+        log.info("connection = {}, class = {}", connection2, connection2.getClass());
+    }
+
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        useDataSource(driverManagerDataSource);
+    }
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
+        Connection connection1 = dataSource.getConnection();
+        Connection connection2 = dataSource.getConnection();
+        log.info("connection = {}, class = {}", connection1, connection1.getClass());
+        log.info("connection = {}, class = {}", connection2, connection2.getClass());
+    }
+}
