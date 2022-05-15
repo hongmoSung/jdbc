23defce6b66f3b896fd1709abe8c434921272c91
hongmoSung <tesung30@naver.com>
2022-05-15 20:00:55 +0900

    feature: 스프링과 문제 해결 - 예외 처리, 반복
    - 런타임 예외 적용

---
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV4_1.java
@@ -0,0 +1,127 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.ex.MyDbException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.support.JdbcUtils;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.NoSuchElementException;
+
+/**
+ * 예외 누수 문제 해결
+ * 체크 예외를 런타임 예외로 변경
+ * MemberRepository 인터페이스 사용
+ * throws SQLException 제거
+ */
+@Slf4j
+public class MemberRepositoryV4_1 implements MemberRepository {
+
+    private final DataSource dataSource;
+
+    public MemberRepositoryV4_1(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public Member save(Member member) {
+        String sql = "insert into member(member_id, money) values (?, ?)";
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, member.getMemberId());
+            preparedStatement.setInt(2, member.getMoney());
+            preparedStatement.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            throw new MyDbException(e);
+        } finally {
+            close(connection, preparedStatement, null);
+        }
+    }
+
+    @Override
+    public Member findById(String memberId) {
+        String sql = "select * from member where member_id = ?";
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, memberId);
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                Member member = new Member();
+                member.setMemberId(resultSet.getString("member_id"));
+                member.setMoney(resultSet.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found member_id = " + memberId);
+            }
+        } catch (SQLException e) {
+            throw new MyDbException(e);
+        } finally {
+            close(connection, preparedStatement, resultSet);
+        }
+    }
+
+    @Override
+    public void update(String memberId, int money) {
+        String sql = "update member set money = ? where member_id = ?";
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, money);
+            preparedStatement.setString(2, memberId);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new MyDbException(e);
+        } finally {
+            close(connection, preparedStatement, null);
+        }
+    }
+
+    @Override
+    public void delete(String memberId) {
+        String sql = "delete from member where member_id = ?";
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, memberId);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new MyDbException(e);
+        } finally {
+            close(connection, preparedStatement, null);
+        }
+    }
+
+    private void close(Connection connection, Statement statement, ResultSet resultSet) {
+        JdbcUtils.closeResultSet(resultSet);
+        JdbcUtils.closeStatement(statement);
+        DataSourceUtils.releaseConnection(connection, dataSource);
+    }
+
+    private Connection getConnection() throws SQLException {
+        Connection connection = DataSourceUtils.getConnection(dataSource);
+        log.info("get connection = {}, class = {}", connection, connection.getClass());
+        return connection;
+    }
+}
